80acdd56686ccb690dba792339a296b85cf2824c
peter738 <tuh10098@temple.edu>
2020-12-01 11:36:15 -0500

    Update react_1.js

--- js/react_1.js
+++ js/react_1.js
@@ -1,4 +1,4 @@
-var img_list = ["/images/location.png", "/images/medical-record.png", "/images/clock.png", "/images/book.png", "/images/medical-file.png"]
+var img_list = ["images\location.png", "images\medical-record.png", "images\clock.png", "images\book.png", "images\medical-file.png"]
 var img_const = ["/images/arrow-left.svg", "/images/arrow-right.svg", "/images/PhillyMap.png.png"]
 var current_feature = ".mapImage"
 
