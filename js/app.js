3844ba153e07e674b5cc789cb9f1241b27dc24e2
Phuc <40041728+thuc753951@users.noreply.github.com>
2020-12-01 03:05:58 -0800

    Delete app.js

--- js/app.js
+++
@@ -1,7 +0,0 @@
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then((reg) => console.log('SW registered',reg))
-    .catch((err) => console.log('service worker not registered',err));
-}
-
